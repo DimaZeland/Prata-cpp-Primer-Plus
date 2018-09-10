b1c1095cbd61fee8e058f56f7a0f428a496dc6b2
Dima Zeland <onenewinvest@gmail.com>
2018-09-10 23:53:48 +0300

    Update MyClass.h

--- Chapter 7 Functions- Cpp-s Programming Modules/MyClass.h
+++ Chapter 7 Functions- Cpp-s Programming Modules/MyClass.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include<algorithm>
 #include <iostream>
 
 using std::cout;
@@ -19,20 +20,25 @@ public:
 class MyClass
 {
 public:
-	MyClass() : itsGolfResult{} {}
+	MyClass() : itsGolfResult{}, itsArr{} {}
 	~MyClass() {}
 	// class methods
 	void DisplayResults(void)const; // display the results of golf games
 	void DisplayBox(const box); // display members of the structure
 	void DisplayBoxMenu(MyClass&, box&); // functionality of work with structure
 	void FactorialMenu(void)const;  //derivation of the factorial of given numbers
+	void Fill_array(double*, int = size); // array filling
 	double FindMeanValue()const; // Find the mean value array's itsGolfResult elements
 	inline double GetAverageHarmonic(double* number1, double* number2)const { // return the average harmonic of two numbers
 		return 2.0 * (*number1) * (*number2) / ((*number1) + (*number2));}
 	int GetFactorial(int)const; // derivation the factorial of given numbers
 	long double	Probability(unsigned, unsigned, unsigned); // taking into account the chance of winning in the American lottery
+	void Reserve_array(double*, int = size); // changing the order of array elements
 	void SetitsGolfResult(void); // set the results of 10 golf games
 	inline void SetBoxVolume(box& one) { one.volume = one.height * one.wight * one.lenght; } // reset the value of structure's memeber "volume"
+	void Show_array(double*, int = size); // deducing the values of array elements
+	static const int size = 10;
+	double itsArr[size];
 private:
 	double itsGolfResult[10]; // include the results of 10 golf games
 };
