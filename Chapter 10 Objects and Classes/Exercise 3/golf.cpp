16433227deedeced4103e45a2e4c6fdf13413358
Dima Zeland <onenewinvest@gmail.com>
2018-10-12 14:47:09 +0300

    Add files via upload

---
+++ Chapter 10 Objects and Classes/Exercise 3/golf.cpp
@@ -0,0 +1,29 @@
+#include "golf.h"
+
+void
+Golf::setgolf(const char * name, int hc) {
+	strcpy_s(fullname, name);
+	itsHandicap = hc;
+}
+
+int
+Golf::setgolf() {
+	std::cout << "Please enter user name: ";
+	std::cin >> fullname;
+	std::cout << "Please enter user handicap: ";
+	std::cin >> itsHandicap;
+	if (' ' == fullname[0] || '\0' == fullname[0])
+		return 0;
+	else
+		return 1;
+}
+
+void
+Golf::handicap(int hc) {
+	itsHandicap = hc;
+}
+
+void
+Golf::showgolf() {
+	std::cout << "\nuser.fullname = " << fullname << ", user.handicap = " << itsHandicap << ".\n";
+}
---
+++ Chapter 10 Objects and Classes/Exercise 3/golf.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+class Golf
+{
+private:
+	static const int Len = 40;
+	int itsHandicap;
+public:
+	char fullname[Len];
+	inline int GetLen() { return Len; }
+	void setgolf(const char * name, int hc);
+	int setgolf();
+	void handicap(int hc);
+	void showgolf();
+};
