3d0c7c999f242c627974244f7a4a160bc9162c44
Dima Zeland <onenewinvest@gmail.com>
2018-10-24 08:24:33 +0300

    Add files via upload

---
+++ Chapter 13 Class Inheritance/Exercise 2/classic.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef CLASSIC_H_
+#define CLASSIC_H_
+#include "cd.h"
+
+class Classic : public Cd
+{
+private:
+	char* mySongName; // main song name
+public:
+	Classic(); // explicit base constructor
+	Classic(const char*, const char *, const char *, int, double); // overload constructor
+	Classic(const Classic&); // explicit copy constructor
+	~Classic(); // explicit base destructor
+	Classic & operator=(const Classic & d); // overloaded assignment operator
+	virtual void Report()const; // reports all CD data
+};
+#endif
